32b8a01b7c67fbfe1aa9d4174a622dcfd83fb222
Dewa Jayon <dewajayon3@gmail.com>
2025-10-15 14:47:13 +0800

    setup login and dashboard page

---
+++ resources/js/Components/HeaderAccount.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+import ProfilePicture from "./ProfilePicture.vue";
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/Components/ui/dropdown-menu";
+</script>
+
+<template>
+    <DropdownMenu>
+        <DropdownMenuTrigger as-child class="outline-none cursor-pointer">
+            <ProfilePicture class="w-9 h-9" />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+            <DropdownMenuLabel>Akun Saya</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <Link :href="route('profile.edit')">
+                <DropdownMenuItem class="cursor-pointer hover:text-black">
+                    Profile
+                </DropdownMenuItem>
+            </Link>
+            <Link
+                :href="route('logout')"
+                as="button"
+                method="post"
+                class="w-full hover:bg-accent hover:text-slate-900 cursor-pointer transition-all ease-in-out duration-300 rounded"
+            >
+                <DropdownMenuItem class="cursor-pointer">
+                    Logout
+                </DropdownMenuItem>
+            </Link>
+        </DropdownMenuContent>
+    </DropdownMenu>
+</template>
--- resources/js/lib/utils.js
+++ resources/js/lib/utils.js
@@ -2,12 +2,16 @@ import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
 }
 
 export function valueUpdater(updaterOrValue, ref) {
-  ref.value =
-    typeof updaterOrValue === "function"
-      ? updaterOrValue(ref.value)
-      : updaterOrValue;
+    ref.value =
+        typeof updaterOrValue === "function"
+            ? updaterOrValue(ref.value)
+            : updaterOrValue;
+}
+
+export function filterMenuByRole(menu, role) {
+    return menu.filter((item) => item.roles.includes(role));
 }
