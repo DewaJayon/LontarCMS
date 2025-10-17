36b62663776d1d39db2bacc40f4c21bb38a9f4a3
Dewa Jayon <dewajayon3@gmail.com>
2025-10-17 22:17:38 +0800

    feat (users): add backend logic feature to create new user
    only admin can create a new user

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Dashboard\DashboardController;
+use App\Http\Controllers\Dashboard\UserController;
 use App\Http\Controllers\Profile\SecurityController;
 use App\Http\Controllers\Profile\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -9,12 +10,17 @@ require __DIR__ . '/auth.php';
 
 Route::middleware('auth')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('home');
-});
 
-Route::middleware('auth')->group(function () {
+    // Profile Route
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/profile/security', [SecurityController::class, 'index'])->name('profile.security');
+
+    // Admin Rute
+    Route::middleware(['auth', 'role:admin'])->group(function () {
+        Route::patch('/users/{user}/reset-password', [UserController::class, 'forceResetPassword'])->name('users.reset-password');
+        Route::resource('users', UserController::class);
+    });
 });
